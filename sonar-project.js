df45816ada69afa3d87c6a6d4419a07f7f3939b4
Delnitolxo <110730000+Delnitolxo@users.noreply.github.com>
2022-09-30 07:34:17 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': '87aed32bfa004875e6888d75c331b963c6554381',
+	    'sonar.login': 'c05decbd834e8b23dc6b04e5bc2916e2572e9247',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
